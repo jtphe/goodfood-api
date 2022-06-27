555baad7d5bc9f9ce4549458952f8a5e43c19235
Soufi Chamalaine <soufi.chamalaine@gmail.com>
2022-06-27 15:13:04 +0200

    RestaurantController correctif 2

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -18,8 +18,8 @@ class RestaurantController extends AbstractController
      * @param Request $request
      * @return JsonResponse
      */
-
-    public function createRestaurant(Request $request, ManagerRegistry $doctrine) {
+    public function createRestaurant(Request $request, ManagerRegistry $doctrine)
+    {
         $em = $doctrine->getManager();
         $restaurant = new Restaurant;
         $restaurantData = json_decode($request->getContent(), true);
@@ -35,5 +35,38 @@ class RestaurantController extends AbstractController
         $em->flush();
 
         return $this->json(['message' => 'restaurant created', "statusCode" => 200]);
+    }
+
+    /**
+    * @Route (name="selectAllRestaurants", path="/api/selectAllRestaurants", methods={"GET"})
+    * @param Request $request
+    * @throws Exception
+    * @return JsonResponse
+    */
+    public function selectAllRestaurants(Request $request, ManagerRegistry $doctrine)
+    {
+        $em = $doctrine->getManager();
+        $prodcuts = $em->getRepository(Restaurant::class)->findAll();
+        return $this->json($prodcuts, 200);
+    }
+
+    /**
+     * @Route (name="filterRestaurants", path="/api/filterRestaurants", methods={"POST"})
+     * @param Request $request
+     * @throws Exception
+     * @return JsonResponse
+     */
+    public function filterRestaurantsByCity(Request $request, ManagerRegistry $doctrine)
+    {
+        $em = $doctrine->getManager();
+        $data = json_decode($request->getContent(), true);
+
+        $city = $data['city'];
+
+        $restaurants = $em->getRepository(Restaurant::class)->findBy(
+            ['city' => $city]
+        );
+
+        return $this->json($restaurants, 200);
     }
 }
